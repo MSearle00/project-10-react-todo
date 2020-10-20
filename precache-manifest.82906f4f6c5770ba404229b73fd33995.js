fcd0a7326e72dd29f44959e3fb223bfa0f58c3de
Maria Searle <maria.searle@deeperthanblue.co.uk>
2020-10-20 12:29:13 +0100

    Updates

---
+++ precache-manifest.82906f4f6c5770ba404229b73fd33995.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "199f89d501cb2fbc09d0b8fb9d16030c",
+    "url": "/project-10-react-todo/index.html"
+  },
+  {
+    "revision": "77aec4ab703b3a1f4020",
+    "url": "/project-10-react-todo/static/css/main.d18dee0f.chunk.css"
+  },
+  {
+    "revision": "11e9e479b2889baeae32",
+    "url": "/project-10-react-todo/static/js/2.16b1ff1a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/project-10-react-todo/static/js/2.16b1ff1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77aec4ab703b3a1f4020",
+    "url": "/project-10-react-todo/static/js/main.f4f512fe.chunk.js"
+  },
+  {
+    "revision": "af1c38f58a1815ac2e9c",
+    "url": "/project-10-react-todo/static/js/runtime-main.42b8a4b3.js"
+  },
+  {
+    "revision": "8dad80289a1ece380591ad143b616747",
+    "url": "/project-10-react-todo/static/media/logo.8dad8028.svg"
+  }
+]);
